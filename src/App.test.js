1b49d13aa2c40ac39ff02376cb0451147356d900
GoofyPR <reddypavan9108@gmail.com>
2023-11-23 08:54:53 +0530

    Deploy my react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main headings', () => {
   render(<App />);
-  screen.debug();
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const completeHeading = screen.getByText(/(\d+)\/(\d+) Complete/i);
+  const messageHeading = screen.getByText(/Try to do at least one!|Nice job for today!|Keep it going/i);
+  
+  expect(completeHeading).toBeInTheDocument();
+  expect(messageHeading).toBeInTheDocument();
 });
+
